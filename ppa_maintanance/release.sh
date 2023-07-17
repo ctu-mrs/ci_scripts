5fbf6223804fc5d39da63c73ed6dcd33eddfc358
Tomas Baca <klaxalk@gmail.com>
2023-07-17 12:34:08 +0200

    separating into subfolders

---
+++ ppa_maintanance/release.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+trap 'echo "$0: \"${last_command}\" command failed with exit code $?"' ERR
+
+$LABEL=$1
+
+echo "$RELEASE_KEY" | gpg --import
+
+sudo apt -y update
+sudo apt -y install dpkg-dev # needed for dpng-scanpackages
+sudo apt -y install apt-utils # needed for apt-ftparchive
+
+# Packages & Packages.gz
+dpkg-scanpackages --multiversion . > Packages
+gzip -k -f Packages
+
+# Release, Release.gpg & InRelease
+apt-ftparchive \
+  -o "APT::FTPArchive::Release::Origin=ctu-mrs" \
+  -o "APT::FTPArchive::Release::Label=$LABEL" \
+  release . > Release
+gpg --default-key "tomas.baca@fel.cvut.cz" -abs -o - Release > Release.gpg
+gpg --default-key "tomas.baca@fel.cvut.cz" --clearsign -o - Release > InRelease
---
+++ ppa_maintanance/squash_master.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+trap 'echo "$0: \"${last_command}\" command failed with exit code $?"' ERR
+
+$PPA=github.com/ctu-mrs/ppa-$1
+
+cd /tmp
+
+git clone https://$PUSH_TOKEN@$PPA repo
+cd repo
+
+git config user.email github@github.com
+git config user.name github
+
+git checkout master
+git checkout --orphan new_master
+git commit -m "Updated packages (squashed)"
+git push origin new_master:master --force
+
+echo "$0: Master squashed"
