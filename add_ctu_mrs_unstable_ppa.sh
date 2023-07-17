964dec51f8a240ae831fb44e52b29429e0ed9cff
Tomas Baca <klaxalk@gmail.com>
2023-07-17 10:17:20 +0200

    updated ppa adders

---
+++ add_ctu_mrs_unstable_ppa.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+trap 'echo "$0: \"${last_command}\" command failed with exit code $?"' ERR
+
+echo "$0: Adding MRS Unstable PPA repository"
+
+curl -s --compressed "https://ctu-mrs.github.io/ppa-unstable/ctu-mrs.gpg" | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/ctu-mrs.gpg >/dev/null
+sudo curl -s --compressed -o /etc/apt/sources.list.d/ctu-mrs.list "https://ctu-mrs.github.io/ppa-unstable/ctu-mrs.list"
+sudo curl -s --compressed -o /etc/ros/rosdep/sources.list.d/30-ctu-mrs.list "https://ctu-mrs.github.io/ppa-unstable/rosdep/ctu-mrs.list"
+sudo apt-get -y update
+
+rosdep update
+
+echo "$0: Finished adding MRS Unstable PPA repository"
