cd0a1206ea2f16f1b7685a484ee2c7ad3749b044
Tomas Baca <klaxalk@gmail.com>
2023-07-19 20:59:31 +0200

    updated ppa adder

--- package_build/add_ctu_mrs_unstable_ppa.sh
+++ package_build/add_ctu_mrs_unstable_ppa.sh
@@ -8,8 +8,9 @@ trap 'echo "$0: \"${last_command}\" command failed with exit code $?"' ERR
 echo "$0: Adding MRS Unstable PPA repository"
 
 curl -s --compressed "https://ctu-mrs.github.io/ppa-unstable/ctu-mrs.gpg" | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/ctu-mrs.gpg >/dev/null
-sudo curl -s --compressed -o /etc/apt/sources.list.d/ctu-mrs.list "https://ctu-mrs.github.io/ppa-unstable/ctu-mrs.list"
-sudo curl -s --compressed -o /etc/ros/rosdep/sources.list.d/30-ctu-mrs.list "https://ctu-mrs.github.io/ppa-unstable/rosdep/ctu-mrs.list"
+sudo curl -s --compressed -o /etc/apt/sources.list.d/ctu-mrs.list "https://ctu-mrs.github.io/ppa-unstable/ctu-mrs-apt.list"
+sudo curl -s --compressed -o /etc/apt/preferences.d/ctu-mrs-ppa-preferences "https://ctu-mrs.github.io/ppa-stable/ctu-mrs-ppa-preferences.txt"
+sudo curl -s --compressed -o /etc/ros/rosdep/sources.list.d/ctu-mrs.list "https://ctu-mrs.github.io/ppa-unstable/ctu-mrs-rosdep.list"
 sudo apt-get -y update
 
 rosdep update
