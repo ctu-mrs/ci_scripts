eef93e5d0748f921ffc0b88fcec44cb532fae2bb
Tomas Baca <klaxalk@gmail.com>
2023-08-23 20:11:14 +0200

    removed git has from onpush builds

--- package_build/build_package.sh
+++ package_build/build_package.sh
@@ -79,8 +79,8 @@ for PACKAGE in $BUILD_ORDER; do
   bloom-generate rosdebian --os-name ubuntu --os-version focal --ros-distro noetic
 
   epoch=1
-  SHA=$(git rev-parse --short HEAD)
-  build_flag=$(date +%Y%m%d.%H%M%S)~git-$SHA
+  # SHA=$(git rev-parse --short HEAD)
+  build_flag=$(date +%Y%m%d.%H%M%S)
 
   sed -i "s/(/($epoch:/" ./debian/changelog
   sed -i "s/)/.${build_flag})/" ./debian/changelog
