60e1820d64c37a1f1fb2574912becb80af4d77b5
Tomas Baca <klaxalk@gmail.com>
2023-08-29 09:03:23 +0200

    updated versioning to epoch2

--- package_build/build_package.sh
+++ package_build/build_package.sh
@@ -78,8 +78,8 @@ for PACKAGE in $BUILD_ORDER; do
   export DEB_BUILD_OPTIONS="parallel=`nproc`"
   bloom-generate rosdebian --os-name ubuntu --os-version focal --ros-distro noetic
 
-  epoch=1
-  build_flag="$(date +%Y%m%d.%H%M%S)~git"
+  epoch=2
+  build_flag="$(date +%Y%m%d.%H%M%S)~on.push.build"
 
   sed -i "s/(/($epoch:/" ./debian/changelog
   sed -i "s/)/.${build_flag})/" ./debian/changelog
