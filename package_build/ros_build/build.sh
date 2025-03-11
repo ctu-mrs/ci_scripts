76607da414a24d594fb332b68b5a1b288f13de50
Tomas Baca <Tomas Baca>
2025-03-11 08:52:11 +0100

    decatkinized the build entrypoint

--- package_build/ros_build/build.sh
+++ package_build/ros_build/build.sh
@@ -27,8 +27,8 @@ REPOSITORY_NAME=$5
 [ -z $BASE_IMAGE ] && BASE_IMAGE=ctumrs/ros_noetic:2025-02-05
 [ -z $DOCKER_IMAGE ] && DOCKER_IMAGE=noetic_builder
 [ -z $ARTIFACTS_FOLDER ] && ARTIFACTS_FOLDER=/tmp/artifacts
-[ -z $REPOSITORY_FOLDER ] && REPOSITORY_FOLDER=/home/klaxalk/git/px4_sitl_gazebo
-[ -z $REPOSITORY_NAME ] && REPOSITORY_NAME=px4_sitl_gazebo
+[ -z $REPOSITORY_FOLDER ] && REPOSITORY_FOLDER=/home/klaxalk/git/mrs_msgs
+[ -z $REPOSITORY_NAME ] && REPOSITORY_NAME=mrs_msgs
 
 ## | ---------------------- derived args ---------------------- |
 
