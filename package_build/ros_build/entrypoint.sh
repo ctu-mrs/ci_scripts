6ca11d703e595f1693309c72ba389b4e9daf2513
Tomas Baca <Tomas Baca>
2025-09-29 13:51:35 +0200

    added rosdep install for buildtool

--- package_build/ros_build/entrypoint.sh
+++ package_build/ros_build/entrypoint.sh
@@ -50,7 +50,7 @@ OLDIFS=$IFS; IFS=$'\n'; for LINE in $BUILD_ORDER; do
 
   apt-get -y update
 
-  rosdep install -y -v --rosdistro=jazzy --dependency-types=build --from-paths ./
+  rosdep install -y -v --rosdistro=jazzy --dependency-types=build --dependency-types=buildtool --from-paths ./
 
   source /opt/ros/jazzy/setup.bash
 
