90cb099c7396dc4f17d4e5667c368069f2a4966b
Tomas Baca <Tomas Baca>
2025-10-17 10:39:38 +0200

    fixing on-push rostest

--- ros_test/entrypoint.sh
+++ ros_test/entrypoint.sh
@@ -15,17 +15,45 @@ echo "$0: installing dependencies using rosdep"
 
 rosdep install -y -v --from-path $WORKSPACE/src || echo "$0: failed to install dependencies using rosdep, the build might fail"
 
+## | ---------------- initialize the workspace ---------------- |
+
+if [ ! -e $WORKSPACE/install ]; then
+
+  echo "$0: workspace not initialized, initializing"
+
+  cd $WORKSPACE
+
+  source /opt/ros/jazzy/setup.bash
+  colcon build --symlink-install
+
+fi
+
+## | -------------------- build the package ------------------- |
+#
+echo "$0: building the workspace"
+
 cd $WORKSPACE
 
-source /opt/ros/jazzy/setup.bash
+source $WORKSPACE/install/setup.bash
+
 colcon build --cmake-args -DENABLE_TESTS=true --paths $WORKSPACE/src/$REPOSITORY_NAME
 
 source $WORKSPACE/install/setup.bash
 
 ## | --- run tests an all ros packages within the repository -- |
 
+echo "$0: running the tests"
+
+cd $WORKSPACE
+
+FAILED=0
+
+colcon test-result --delete-yes
+
 colcon test --base-paths $WORKSPACE/src/$REPOSITORY_NAME
 
-colcon test-result --all --verbose
+colcon test-result --all --verbose || FAILED=1
 
 echo "$0: tests finished"
+
+exit $FAILED
