7490edd8ee8bd14ad1d58e6091c914d1c966eb4d
Vojtech Spurny <spurny2@gmail.com>
2026-02-04 12:29:10 +0100

    add rosdep ignore for dependency in the workspace

--- package_build/ros_build/entrypoint.sh
+++ package_build/ros_build/entrypoint.sh
@@ -59,7 +59,7 @@ OLDIFS=$IFS; IFS=$'\n'; for LINE in $BUILD_ORDER; do
 
   apt-get -y update
 
-  rosdep install -y -v --rosdistro=jazzy --dependency-types=build --dependency-types=buildtool --from-paths ./
+  rosdep install -y -v --rosdistro=jazzy --dependency-types=build --dependency-types=buildtool --from-paths ./ --ignore-src -y 
 
   source /opt/ros/jazzy/setup.bash
 
